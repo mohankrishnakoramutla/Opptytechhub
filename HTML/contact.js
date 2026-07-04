71e6180945c65c79e9aeee8d6b954bcc991ed210
vanipenta saleem <vanipentashaiksaleem@gmail.com>
2026-07-04 16:45:08 +0530

    push

---
+++ HTML/contact.js
@@ -0,0 +1,40 @@
+const contactForm = document.getElementById("contactForm");
+const successMessage = document.getElementById("formSuccess");
+
+contactForm.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    // Get form values
+    const formData = {
+        full_name: document.getElementById("ct-name").value,
+        email: document.getElementById("ct-email").value,
+        phone: document.getElementById("ct-phone").value,
+        service: document.getElementById("ct-service").value,
+        subject: document.getElementById("ct-subject").value,
+        message: document.getElementById("ct-msg").value
+    };
+
+    try {
+        const response = await fetch("http://127.0.0.1:8000/api/contact/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            successMessage.style.display = "flex";
+            contactForm.reset();
+            console.log("Success:", data);
+        } else {
+            alert(data.error || "Something went wrong");
+        }
+
+    } catch (error) {
+        console.error("Error:", error);
+        alert("Server error");
+    }
+});
