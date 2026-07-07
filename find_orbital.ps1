fdb0d14daf0d77acc019f6d9f341ce1e9c5f1f7b
mohankrishnakoramutla <mohanoppty@gmail.com>
2026-07-07 18:33:26 +0530

    mk60

--- find_orbital.ps1
+++
@@ -1,6 +0,0 @@
-$l = Get-Content "c:\Users\santh\OneDrive\Documents\GitHub\Opptytechhub\index.html" -Encoding UTF8
-for($i=0; $i -lt $l.Length; $i++) {
-    if($l[$i] -like '*wo-visual*' -or $l[$i] -like '*wo-badge*' -or $l[$i] -like '*wo-container*' -or $l[$i] -like '*wo-rings*') {
-        Write-Host "$($i+1): $($l[$i])"
-    }
-}
